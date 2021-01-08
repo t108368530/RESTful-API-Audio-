62b5a2f04349c773813b395d9a80324c6a9edf18
alien <t108368530@ntut.org.tw>
2021-01-08 14:48:15 +0800

    update

--- src/webservice/nn/testredis.py
+++
@@ -1,9 +0,0 @@
-from webservice.core.config import message_queue2, settings
-import os
-path = os.getcwd()
-print(path)
-print(settings.REDUS_UNIX_SOCKET_PATH)
-# settings.REDUS_UNIX_SOCKET_PATH = "../../docker/redis/redis-socket/redis.sock"
-# /Users/mbp/Documents/my-project/python-snippets/notebook
-print(message_queue2.ping())
-# message_queue2.s
